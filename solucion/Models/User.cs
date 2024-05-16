788f7f5cffcf3c7324a67e104a0812bc4bab7831
CristhianDavid <emmacris04@gmail.com>
2024-05-15 20:45:50 -0500

    feat: Add User Models

---
+++ solucion/Models/User.cs
@@ -0,0 +1,13 @@
+namespace solucion.Models
+{
+    public class User
+    {
+        public int Id { get; set;}
+        public string? Name { get; set;}
+        public string? LastName { get; set;}
+        public string? Email { get; set; }
+        public int Age { get; set; }
+
+    }
+
+}
