53cfa73a2bc3f34a0e8c5afab22878e2afe2a0dd
CristhianDavid <emmacris04@gmail.com>
2024-05-17 06:16:39 -0500

    feat: Add Controllers HTTP

--- solucion/Controllers/UsersController.cs
+++ solucion/Controllers/UsersController.cs
@@ -15,11 +15,72 @@ namespace solucion.Controllers;
             _context = context;
         }
 
+
+        //Listado de todos los usuarios
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> ListarUsuarios()
         {
             return await _context.users.ToListAsync();
         }
+
+        //Detalles de un usuario en especifico
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> UsuarioId(int id)
+        {
+            var user = await _context.users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+
+        //Crear Usuario
+        [HttpPost]
+        public async Task<ActionResult<User>> CrearUsuario(User user)
+        {
+            _context.users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new {id = user.Id}, user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> EliminarUsuario(int id)
+        {
+            var user = await _context.users.FindAsync(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            _context.users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent(); 
+        }
+
+        //Actualizar
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> ActualizarUsuario(int id, User user)
+        {
+            var usuario = await _context.users.FindAsync(id);
+            if(usuario == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Entry(user).State = EntityState.Modified; //Entry= busacar en el modelo y sobreescribe state= cambio el estado pero no se almacenado Entitystate.Modifi= modifica
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new {id = user.Id}, user);
+
+        }
+
 
     }
 
